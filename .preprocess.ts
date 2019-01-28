e86597f3f8859ebd3e983ec5c95d0cc86da5725e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-01-28 13:46:28 +0100

    feat: move monitoring from wise hub to separate dir

---
+++ .preprocess.ts
@@ -0,0 +1,12 @@
+// this file is loaded by wise-ci preprocessor
+
+export const hooks = [];
+export const data = {
+    repository: {
+        name: "dockerized-mocha-slack-service-monitoring"
+    },
+    npm: {
+        package: "dockerized-mocha-slack-service-monitoring"
+    }
+};
+export const excludes = [ "dist", ".vscode", "coverage" ];
